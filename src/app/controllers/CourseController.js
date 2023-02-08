d339b2377f95b530f42129cb44890fcb198cf7a7
DC-VIETIS\QuyenVV <quyenvv@vietis.com.vn>
2023-02-08 16:05:50 +0700

    08/02/2023

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,13 +1,55 @@
 const Course = require("../models/Course");
-const { multipleMongooseToObject } = require("../../util/mongoose");
+const { mongooseToObject } = require("../../util/mongoose");
 
 class CourseController {
-    // [GET] /show
+    // [GET] courses/show
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
-            .then((course) => {
-                res.render("../../resources/courses/show.hbs");
-            })
+            .then((course) =>
+                res.render("../../resources/courses/show.hbs", {
+                    course: mongooseToObject(course),
+                })
+            )
+            .catch(next);
+    }
+    // [GET] courses/Create
+    create(req, res, next) {
+        res.render("../../resources/courses/create.hbs");
+    }
+
+    // [POST] courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD-FIVd-JVbRkLycgNDndtfoVNCFA`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect("/"))
+            .catch((err) => {});
+    }
+
+    // [GET] /courses/:id/edit
+    edit(req, res, next) {
+        Course.findById(req.params.id)
+            .then((course) =>
+                res.render("../../resources/courses/edit.hbs", {
+                    course: mongooseToObject(course),
+                })
+            )
+            .catch(next);
+    }
+
+    // [PUT] /courses/:id
+    update(req, res, next) {
+        Course.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect("/me/stored/courses"))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect("back"))
             .catch(next);
     }
 }
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const slug = require("mongoose-slug-generator");
 
-const Course = new Schema({
-    name: { type: String, default: "", maxLength: 255 },
-    description: { type: String, default: "", maxLength: 600 },
-    image: { type: String, default: "", maxLength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-});
+mongoose.plugin(slug);
+
+const Course = new Schema(
+    {
+        name: { type: String, default: "", maxLength: 255, required: true },
+        description: { type: String, default: "", maxLength: 600 },
+        videoId: { type: String, default: "", required: true, maxLength: 255 },
+        level: { type: String, default: "", maxLength: 255 },
+        image: { type: String, default: "", maxLength: 255 },
+        slug: { type: String, slug: "name", unique: true },
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("Course", Course);
